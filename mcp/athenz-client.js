0ed83f1c2153ec9f8c0d01cab9e188510cd31352
Aaron Jeongwoo Kim <53258958+mlajkim@users.noreply.github.com>
2026-04-25 11:42:14 +0900

    Feat: `make mcp-local` to run server (#3)
    * feat
    
    Signed-off-by: Jeongwoo Kim - jekim <jekim@lycorp.co.jp>
    
    * feat
    
    Signed-off-by: Jeongwoo Kim - jekim <jekim@lycorp.co.jp>
    
    * doc
    
    Signed-off-by: Jeongwoo Kim - jekim <jekim@lycorp.co.jp>
    
    * feat
    
    Signed-off-by: Jeongwoo Kim - jekim <jekim@lycorp.co.jp>
    
    * feat
    
    Signed-off-by: Jeongwoo Kim - jekim <jekim@lycorp.co.jp>
    
    * feat
    
    Signed-off-by: Jeongwoo Kim - jekim <jekim@lycorp.co.jp>
    
    * feat
    
    Signed-off-by: Jeongwoo Kim - jekim <jekim@lycorp.co.jp>
    
    * feat
    
    Signed-off-by: Jeongwoo Kim - jekim <jekim@lycorp.co.jp>
    
    ---------
    
    Signed-off-by: Jeongwoo Kim - jekim <jekim@lycorp.co.jp>

---
+++ mcp/athenz-client.js
@@ -0,0 +1,69 @@
+import fs from "fs";
+import https from "https";
+
+const ATHENZ_CONF = {
+  zts: {
+    hostname: "localhost",
+    port: 8443,
+  },
+  certPath: "../../athenz_dist/certs/athenz_admin.cert.pem",
+  keyPath: "../../athenz_dist/keys/athenz_admin.private.pem",
+  scope: "api:role.docs-getter api:role.docs-poster", // emtpy space separated string
+};
+
+let cachedToken = null;
+let expiryTime = 0;
+
+export async function getAthenzToken() {
+  const now = Date.now();
+  
+  if (cachedToken && expiryTime > now + 60000) {
+    return cachedToken;
+  }
+
+  return new Promise((resolve, reject) => {
+    const params = new URLSearchParams({
+      grant_type: "client_credentials",
+      scope: ATHENZ_CONF.scope,
+      expires_in: "3600"
+    });
+
+    const options = {
+      hostname: ATHENZ_CONF.zts.hostname,
+      port: ATHENZ_CONF.zts.port,
+      path: `/zts/v1/oauth2/token`,
+      method: "POST",
+      key: fs.readFileSync(ATHENZ_CONF.keyPath),
+      cert: fs.readFileSync(ATHENZ_CONF.certPath),
+      rejectUnauthorized: false,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": Buffer.byteLength(params.toString()),
+      },
+      timeout: 10000,
+    };
+
+    const req = https.request(options, (res) => {
+      let data = "";
+      res.on("data", (chunk) => (data += chunk));
+      res.on("end", () => {
+        if (res.statusCode === 200) {
+          const result = JSON.parse(data);
+          cachedToken = result.access_token;
+          expiryTime = now + (result.expires_in * 1000);
+          resolve(cachedToken);
+        } else {
+          reject(new Error(`Athenz auth failed: ${res.statusCode} ${data}`));
+        }
+      });
+    });
+
+    req.on("error", (err) => {
+      console.error("Request Error:", err);
+      reject(err);
+    });
+
+    req.write(params.toString());
+    req.end();
+  });
+}
